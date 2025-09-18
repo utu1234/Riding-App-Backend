06239c5ad3ea815adf2d8b5548b02a22e8b90a19
utu964355 <singhutkarsh9643@gmail.com>
2025-09-18 18:52:41 +0530

    temp rename for blackListToken.model.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-const blacklistTokenModel = require('../models/blacklistToken.model');
+const blacklistTokenModel = require('../models/blackListToken.model');
 const bcrypt = require("bcryptjs");
 
 module.exports.registerUser = async (req, res, next) => {
